70ef3675637c074cf46bf73dfca17a30d9d0be11
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-04-10 23:02:08 -0300

    Add gateway API index and configuration files

---
+++ gateway/src/index.ts
@@ -0,0 +1,21 @@
+import express, {
+  Request,
+  Response,
+  Express
+} from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+
+dotenv.config();
+
+const app: Express = express();
+app.use(morgan("dev"));
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Gateway server is running on port ${port}`);
+});
