a19ed094ac70fe7de15acc4b4e92a51cd4e5b791
Matías Henríquez <matias.henriquez.dev@gmail.com>
2024-04-11 20:44:23 -0300

    Update ESLint and Prettier configurations for gateway API

--- gateway/.eslintrc.js
+++ gateway/.eslintrc.js
@@ -21,30 +21,5 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off'
-  },
-  jest: {
-    moduleFileExtensions: ['js', 'json', 'ts'],
-    rootDir: 'src',
-    moduleNameMapper: {
-      '@modules/(.*)$': '<rootDir>/modules/$1',
-      '@common/(.*)$': '<rootDir>/common/$1',
-      '^@root/(.*)$': '<rootDir>/../$1',
-      '^@data/(.*)$': '<rootDir>/../data/$1',
-      '^@/(.*)$': '<rootDir>/../src/$1',
-      '@configuration/(.*)$': '<rootDir>/configuration/$1'
-    },
-    testRegex: '.*\\.spec\\.ts$',
-    transform: {
-      '^.+\\.(t|j)s$': 'ts-jest'
-    },
-    collectCoverageFrom: ['**/*.(t|j)s'],
-    coverageDirectory: '../coverage',
-    testEnvironment: 'node',
-    testResultsProcessor: 'jest-sonar-reporter'
-  },
-  jestSonar: {
-    reportPath: 'coverage',
-    reportFile: 'test-reporter.xml',
-    indent: 4
   }
 };
