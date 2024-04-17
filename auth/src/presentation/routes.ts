d51b31cfd9df5a7fb87b6649c33aa62805b074cb
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-04-17 19:39:16 -0300

    Add validation for CreateUserDto in /register route

--- auth/src/presentation/routes.ts
+++ auth/src/presentation/routes.ts
@@ -1,12 +1,17 @@
 import express, { Router } from 'express';
 import type { Request, Response } from 'express';
 import { UserService } from '../application/services/user.service';
+import { IUserService } from '../application/interfaces/user-service.interface';
+import { CreateUserDto } from '../application/dtos/requests/create-user.dto';
+import { validate } from 'class-validator';
+import { HttpError } from '../utils/httpError';
 
 export class AuthRoutes {
   private router: Router;
-  private readonly userService = new UserService();
+  private readonly userService: IUserService;
 
   constructor() {
+    this.userService = new UserService();
     this.router = express.Router();
     this.routes();
   }
@@ -22,11 +27,25 @@ export class AuthRoutes {
     });
 
     this.router.post('/register', async (req: Request, res: Response) => {
-      const createdUser = await this.userService.createUser(
-        req.body.email,
-        req.body.password
-      );
-      res.status(201).json(createdUser);
+      const { email, password } = req.body;
+      const user = new CreateUserDto(email, password);
+
+      const errors = await validate(user);
+      if (errors.length > 0) {
+        res.status(400).json(errors);
+      } else {
+        try {
+          const createdUser = await this.userService.createUser({
+            email,
+            password
+          });
+          res.status(201).json(createdUser);
+        } catch (error) {
+          if (error instanceof HttpError)
+            res.status(error.statusCode).json({ error: error.message });
+          else res.status(500).json({ error: 'Internal server error' });
+        }
+      }
     });
 
     this.router.post(
