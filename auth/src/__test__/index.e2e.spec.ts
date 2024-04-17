4249bd7bafb7f5ac53a369dfd0f97c6c6b7a721f
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-04-17 09:19:22 -0300

    Refactor server import in index.e2e.spec.ts

--- auth/src/__test__/index.e2e.spec.ts
+++ auth/src/__test__/index.e2e.spec.ts
@@ -1,17 +1,13 @@
 import request from 'supertest';
-import { server } from '../index';
+import app from '../index';
 
 describe('GET /', () => {
   it('should return "Hello World!"', async () => {
-    const response = await request(server).get('/');
+    const response = await request(app).get('/');
 
     expect(response.status).toBe(200);
     expect(response.text).toBe('Auth service is running');
 
     return;
-  });
-
-  afterAll((done) => {
-    server.close(done);
   });
 });
