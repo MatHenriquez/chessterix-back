cffcc95c6e0158585563a3351575e8f3cc4b1ffa
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-04-24 20:55:05 -0300

    Refactor the Gateway index file

--- gateway/src/index.ts
+++ gateway/src/index.ts
@@ -9,9 +9,7 @@ app.use(morgan('dev'));
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.send('Chessterix API');
 });
 
-export const server = app.listen(port, () => {
-  console.log(`Gateway server is running on port ${port}`);
-});
+export default app;
