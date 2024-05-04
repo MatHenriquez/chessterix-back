f25b421c47a27db4e76d07934ede510901b328cc
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-05-04 00:04:32 -0300

    Add e2e tests for login endpoint

---
+++ auth/src/__tests__/auth.e2e.spec.ts
@@ -0,0 +1,59 @@
+import request from 'supertest';
+import app from '../index';
+import { initializeDb, closeDb } from '../config/orm.config';
+
+describe('Auth routes', () => {
+  beforeAll(async () => {
+    await initializeDb();
+  });
+
+  afterAll(async () => {
+    closeDb();
+  });
+
+  describe('POST /login', () => {
+    it('should return 400 if email or password are not provided', async () => {
+      const response = await request(app).post('/login').send({});
+
+      expect(response.status).toBe(400);
+      expect(response.body).toEqual({
+        error: 'Email and password are required'
+      });
+
+      return;
+    });
+
+    it('should return 404 if user is not found', async () => {
+      const response = await request(app).post('/login').send({
+        email: 'fake@mail.com',
+        password: 'Password135.'
+      });
+
+      expect(response.status).toBe(404);
+      expect(response.body).toEqual({ error: 'User not found' });
+
+      return;
+    });
+
+    it('should return 401 if password is invalid', async () => {
+      const response = await request(app).post('/login').send({
+        email: 'john@doe.com',
+        password: 'Password135..'
+      });
+
+      expect(response.status).toBe(401);
+      expect(response.body).toEqual({ error: 'Invalid password' });
+    });
+
+    it('should return 200 with token if user is found and password is valid', async () => {
+      const response = await request(app).post('/login').send({
+        email: 'john@doe.com',
+        password: 'Password135.'
+      });
+
+      expect(response.status).toBe(200);
+      expect(response.body.error).toBeNull();
+      expect(response.body.data.token).toEqual(expect.any(String));
+    });
+  });
+});
